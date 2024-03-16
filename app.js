5c4ffbf1235bd081543b92df4940d59398ebaa89
shekhargrace14 <shekhargrace14@gmail.com>
2024-03-16 23:48:20 +0530

    addToCart added

--- app.js
+++ app.js
@@ -47,8 +47,23 @@ listProductHTML.addEventListener('click', (event)=>{
     }
 })
 const addToCart = (product_id)=>{
-
+    let positionThisProductInCart = carts.findIndex((value)=>value.product_id == product_id);  
+    if(carts.length <=0){
+        carts = [{
+            product_id: product_id,
+            quantity: 1
+        }]
+    }else if(positionThisProductInCart < 0){
+        carts.push({
+            product_id: product_id,
+            quantity:1
+        })
+    }else{
+        carts[positionThisProductInCart].quantity = carts[positionThisProductInCart].quantity + 1;
+    }
+    addCartToHTML();
 }
+
 const initApp = () =>{
     // get data from 
     fetch('https://fakestoreapi.com/products')
