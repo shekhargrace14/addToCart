27794eec7ca0b00c411b71991047a1d9eee51804
shekhargrace14 <shekhargrace14@gmail.com>
2024-03-17 23:34:14 +0530

    code need debugging

--- app.js
+++ app.js
@@ -70,11 +70,14 @@ const addCartToMemory=()=>{
 const addCartToHTML = () =>{
     listCartHTML.innerHTML = '';
     let totalQuantity = 0;
-    if(carts.length>0){
+    if(carts.length > 0){
         carts.forEach(cart =>{
             totalQuantity = totalQuantity + cart.quantity;
             let newCart = document.createElement('div');
+
             newCart.classList.add('item');
+            newCart.dataset.id = cart.product.id;
+
             let positionProduct = listProducts.findIndex((value)=> value.id == cart.product_id);
             let info = listProducts[positionProduct];
             newCart.innerHTML= `
@@ -104,6 +107,39 @@ const addCartToHTML = () =>{
         })
     }
     iconCartSpan.innerText = totalQuantity;
+}
+listCartHTML.addEventListener('click',(event)=>{
+    let positionClick = event.target;
+    if(positionClick.classList.contains('minus')|| positionClick.classList.contains('minus')){
+        let product_id = positionClick.parentElement.dataset.id;
+        let type = 'minus'
+        if(positionClick.classList.contains('plus')){
+            type = 'plus'
+        }
+        changeQuantity(product_id, type);
+    }
+})
+
+const changeQuantity = (product_id, type)=>{
+    let positionItemInCart = carts.findIndex((value)=> value.product_id == product_id)
+    if(positionItemInCart >= 0){
+        switch(type){
+            case 'plus':
+                carts[positionItemInCart].quantity = carts[positionItemInCart].quantity + 1;
+                break;
+            default:
+                let valueChange = carts[positionItemInCart].quantity - 1;
+                if(valueChange > 0){
+                    carts[positionItemInCart].quantity = valueChange;
+                }else{
+                    carts.splice(positionItemInCart, 1 )
+                }
+                break;
+        }
+    }
+    addCartToMemory();
+    addCartToHTML();
+
 }
 const initApp = () =>{
     // get data from 
