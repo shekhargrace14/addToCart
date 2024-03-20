e35e058466d719bcb283270d361cf5fb1e497528
shekhargrace14 <shekhargrace14@gmail.com>
2024-03-20 23:42:56 +0530

    my try created

---
+++ myTry/script.js
@@ -0,0 +1,53 @@
+// categories 1. 
+
+let header = document.querySelector("header")
+
+
+fetch("https://fakestoreapi.com/products")
+.then(response=>response.json())
+.then(data=>{
+    // console.log(data);
+    let productsRow = document.querySelector(".productsRow");
+    data.map((value,index)=>{
+        productsRow.insertAdjacentHTML("beforeend",`
+        <div class="column card">
+                <div class="column cardBody" id=${value.id}>
+                    <div class="utilityIcons d-flex direction-column">
+                        <i class="fa-regular fa-heart"></i>
+                        <i class=" onHover fa-solid fa-magnifying-glass"></i>
+                    </div>
+                    <div class="imgBox">
+                        <img src=${value.image} alt="">
+
+                    </div>                   
+                    <div class="info">
+                        <p class="rating"> ${value.rating.rate} | ${value.rating.count} </p>
+                        <h3 class="productTitle">${value.title}</h3>
+                        
+                        <div class="price d-flex ">
+                        <p class="CurrentPrice">$ ${value.price.toFixed(2)}</p>
+                        <p class="discount">$ ${(value.price+ 10).toFixed(2) }</p>
+                        </div>
+                    </div>
+                    <div >
+                   
+                        <button class="btn-primary" id=${value.id}>Quick Shop</button>
+                       
+
+                    </div>
+
+                </div>
+        </div>
+        `)
+        // console.log(value.title)
+    })
+    let card = document.querySelectorAll(".card")
+    console.log(card, "card");
+    card.forEach((item,index)=>{
+        item.addEventListener("click", ()=>{
+            let itemID = item.firstElementChild.id
+            console.log(itemID, "card has been clicked")
+            localStorage.setItem("singleProductPage",itemID)
+        })
+    })
+})
